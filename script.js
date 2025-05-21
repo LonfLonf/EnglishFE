7541a66d2efd5e7058598a704a196e32b32d0786
Yuri Souza <yuri_souzaln@outlook.com>
2025-05-21 00:41:47 -0300

    i'm lazy, tired, no motivation, sad, loneliness, i don't know js, html, css, i hate web dev, i'm stupid as fuck

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ document.getElementById("mostrar").addEventListener("click", () => {
     resultado.textContent = "Por favor, digite seu nome.";
   }
 });
+
+function showLogin() {
+  console.log("Login");
+  document.getElementById("loginForm").classList.add("hidden");
+  document.getElementById("registerForm").classList.remove("hidden");
+}
+
+function showRegister() {
+  console.log("Registeer");
+  document.getElementById("registerForm").classList.add("hidden");
+  document.getElementById("loginForm").classList.remove("hidden");
+}
